29f1b8a9b9780d166a6274fffa930b38ae4f665d
Marc-Andre Ferland <madrang@gmail.com>
2011-11-05 23:51:51 -0400

    app menu os now a dock.

--- printApps.js
+++ printApps.js
@@ -8,35 +8,49 @@ function initApps()
 
 function printApps()
 {
-	$('body div.apps').empty();
-	$('body div.apps').append('<h2 class="appsHeader">Applications</h2>');
+	$('body div.dock-container').empty();
+	
+	// Add other menu items.
+	$('body div.dock-container').append('<a class="dock-item" id="optionlink" href="options.html">' +
+		'<img src="images/Wrench_White.png" alt="home" />' +
+		'<span>Options</span>' +
+		'</a>');
 	
 	var func = function(extensions) {
 		for (var i in extensions) {
 			if(extensions[i].isApp)
-				$('body div.apps').append(printApplication(extensions[i]));
+				$('body div.dock-container').append(builDockItem(extensions[i]));
 		}
+		
+		$('#dock').Fisheye(
+			{
+				maxWidth: 50,
+				items: 'a',
+				itemsText: 'span',
+				container: '.dock-container',
+				itemWidth: 40,
+				proximity: 90,
+				halign : 'center'
+			}
+		)
 	};
 	
 	chrome.management.getAll(func);
 }
 
-function printApplication(extInf)
+function builDockItem(extInf)
 {
-	var item = $('<div id="' + extInf.id + '" class="app"></div>');
+	var item = $('<a id="' + extInf.id + '" class="dock-item"></a>');
 	
+	//App icon
 	var img = (extInf.icons) ?
-		//App icon
-		$('<img id="img_'+ extInf.id +'" src="' +
-		((extInf.enabled) ?
-			getBiggerImg(extInf.icons).url :
-			' '
-		) + '" width="128px" height="128px" alt="" />')
+		//(extInf.enabled) is false dim im gray.
+		$('<img id="img_'+ extInf.id +'" src="' + getBiggerImg(extInf.icons).url + '"/>')
 		//Default icon.
 		: $('');
 	
 	item.append(img);
-	item.append($('<a class="appLabel">' + extInf.name + '</a>'));
+	item.append($('<span>' + extInf.name + '</span>'));
 	
 	item.click({ ext: extInf }, function(e) {
 		chrome.management.launchApp(e.data.ext.id);
