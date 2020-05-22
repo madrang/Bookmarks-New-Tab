4def370f440d17bef1e41c072bf85f95aa338315
James Babcock <jimrandomh@gmail.com>
2020-05-22 15:11:58 -0700

    Add an option to hide the apps dock

--- options.js
+++ options.js
@@ -8,12 +8,16 @@ function initOptionsPage()
 {
 	$("#jsTreeTheme-Menu").val(localStorage.jsTree_theme);
 	$("#jsTreeTheme-Dots")[0].checked = parseBool(localStorage.jsTree_themeDots);
+	$("#hideApps")[0].checked = parseBool(localStorage.hideApps);
 
 	$("#jsTreeTheme-Menu").change(function(ev) {
 		localStorage.jsTree_theme = $(ev.target).val();
 	});
 	$("#jsTreeTheme-Dots").change(ev => {
 		localStorage.jsTree_themeDots = $(ev.target)[0].checked;
+	});
+	$("#hideApps").change(ev => {
+		localStorage.hideApps = $(ev.target)[0].checked;
 	});
 }
 
